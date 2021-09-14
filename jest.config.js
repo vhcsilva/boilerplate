2940cb91acfad9c846e7fe4501e4496882d6a7fc
Vitor Hugo <vitor.silva@asabranca.ind.br>
2021-09-13 22:47:49 -0300

    updating

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: [
+    'components/**/*.ts(x)',
+    '!components/**/stories.tsx',
+    'pages/**/*.ts(x)',
+    'styles/**/*.ts(x)'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
